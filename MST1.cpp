d30e7d8cc0b148b681f197eaa0e2f2a4dc7a877d
Pilleteer <patcharapol.ra@ku.th>
2023-02-19 11:12:51 +0700

    "update"

---
+++ MST1.cpp
@@ -0,0 +1,72 @@
+#include<iostream>
+#include<string>
+#include<list>
+#include<vector>
+using namespace std;
+
+const int MAXN = 100010;    
+int n,m;
+int edges[MAXN][2];
+int weight[MAXN];
+pair<int,int> sorted_edges[MAXN];
+int parent[MAXN];
+
+void read_input(){
+    cin >> n >> m;
+    for(int i=0;i<m;i++){
+        int a,b;
+        cin >> a >> b >> weight[i];
+        edges[i][0]=a-1;
+        edges[i][1]=b-1;
+        sorted_edges[i].first=weight[i];
+        sorted_edges[i].second=i;
+    }
+}
+
+void sort_edges(){
+    for(int i=0;i<m;i++){
+        for(int j=i+1;j<m;j++){
+            if(sorted_edges[i].first>sorted_edges[j].first){
+                swap(sorted_edges[i],sorted_edges[j]);
+            }
+        }
+    }
+}
+
+int find(int x){
+    if(parent[x]==x){
+        return x;
+    }
+    return parent[x]=find(parent[x]);
+}
+
+void union_set(int x,int y){
+    int px=find(x);
+    int py=find(y);
+    if(px!=py){
+        parent[px]=py;
+    }
+}
+
+void init(){
+    for(int i=0;i<n;i++){
+        parent[i]=i;
+    }
+}
+
+int main(){
+    read_input();
+    sort_edges();
+    init();
+    int sum=0;
+    for(int i=0;i<m;i++){
+        int a=edges[sorted_edges[i].second][0];
+        int b=edges[sorted_edges[i].second][1];
+        if(find(a)!=find(b)){
+            union_set(a,b);
+            sum+=sorted_edges[i].first;
+        }
+    }
+    cout << sum << endl;
+    return 0;
+}
